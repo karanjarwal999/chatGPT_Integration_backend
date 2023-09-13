4427fa9bc6014c2ba52262b0f78cbd4594bd4b61
karanjarwal999 <karanjarwal999@gmail.com>
2023-09-13 12:06:52 +0530

    ok

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+var cors = require('cors')
 const generateResponse = require('./generateResponse');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('chatGPT Integrated on end point /generate-response  *pass promt in body')
